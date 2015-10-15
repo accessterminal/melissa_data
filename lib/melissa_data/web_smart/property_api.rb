3ee87c19ce7ec5a8e012f1e42691a8a0013729ab
Ybur Yug <bobbygrayson@gmail.com>
2015-10-15 15:35:57 -0400

    setup address key api lookup

--- lib/melissa_data/web_smart/property_api.rb
+++ lib/melissa_data/web_smart/property_api.rb
@@ -4,12 +4,30 @@ require 'nokogiri'
 module MelissaData
   module WebSmart
     class PropertyAPI
-      def property(fips:, apn:)
+      def property_by_apn(fips:, apn:)
         resp = RestClient.get('https://property.melissadata.net/v3/REST/Service.svc/doLookup',
                              { params: { id: MelissaData.web_smart_id,
                                          fips: fips,
                                          apn: apn } })
         PropertyXMLParser.new(Nokogiri::XML(resp)).parse
+      end
+
+      def property_by_address_key(address_key:)
+        resp = RestClient.get('https://property.melissadata.net/v3/REST/Service.svc/doLookup',
+                             { params: { id: MelissaData.web_smart_id,
+                                         AddressKey: address_key } })
+        PropertyXMLParser.new(Nokogiri::XML(resp)).parse
+      end
+
+      def address(address:, city:, state:, zip:, country:)
+        resp = RestClient.get('https://address.melissadata.net/v3/WEB/GlobalAddress/doGlobalAddress',
+                             { params: { id: MelissaData.web_smart_id,
+                                         a1: address,
+                                         loc: city,
+                                         admarea: state,
+                                         postal: zip,
+                                         ctry: country} })
+        AddressXMLParser.new(Nokogiri::XML(resp)).parse
       end
     end
   end
