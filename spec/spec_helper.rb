634582e34094105367e56c32622a869cf7a2a48d
Jonah Ruiz <jonah@pixelhipsters.com>
2015-10-18 15:12:13 -0400

    Clean up stubs formatting inside spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,22 +14,20 @@ RSpec.configure do |config|
               :headers => {})
 
     stub_request(:get, /property/).
-    with(:headers =>
-         {'Accept'=>'*/*; q=0.5, application/xml',
-          'Accept-Encoding'=>'gzip, deflate',
-          'User-Agent'=>'Ruby'}).
+    with(:headers => {'Accept'=>'*/*; q=0.5, application/xml',
+                      'Accept-Encoding'=>'gzip, deflate',
+                      'User-Agent'=>'Ruby'}).
     to_return(:status => 200,
-            :body => File.read("spec/fixtures/fips_apn_response.xml"),
-            :headers => {})
+              :body => File.read("spec/fixtures/fips_apn_response.xml"),
+              :headers => {})
 
     stub_request(:get, "https://property.melissadata.net/v3/REST/Service.svc/doLookup?AddressKey=4342099&id=").
     with(:headers => { 'Accept'=>'*/*; q=0.5, application/xml',
                        'Accept-Encoding'=>'gzip, deflate',
                        'User-Agent'=>'Ruby'}).
-    to_return(
-      :status => 200,
-      :body => File.read("spec/fixtures/address_error_response.xml"),
-      :headers => {})
+    to_return(:status => 200,
+              :body => File.read("spec/fixtures/address_error_response.xml"),
+              :headers => {})
   end
 
   config.expect_with :rspec do |expectations|
