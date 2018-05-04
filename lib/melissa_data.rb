559362a602bd15d62bb3dda1cb59146238d03ca9
Jonah Ruiz <jonah@pixelhipsters.com>
2018-05-04 16:09:05 -0400

    Add active_support/core_ext/hash
      - Remove nokogiri

--- lib/melissa_data.rb
+++ lib/melissa_data.rb
@@ -1,3 +1,4 @@
+require "active_support/core_ext/hash"
 require "melissa_data/version"
 require "melissa_data/web_smart/xml"
 require "melissa_data/web_smart/response_processor"
@@ -7,7 +8,6 @@ require "melissa_data/config"
 require "melissa_data/geo_lookup/geocoder"
 
 require 'rest-client'
-require 'nokogiri'
 require 'geokit'
 
 module MelissaData
