c197f008a9819b556da5bd4b9567fc1ead20b463
Jonah Ruiz <jonah@pixelhipsters.com>
2018-05-04 16:59:46 -0400

    Rewrite http calls with Faraday

--- lib/melissa_data/web_smart/property_api.rb
+++ lib/melissa_data/web_smart/property_api.rb
@@ -1,41 +1,65 @@
-require 'rest-client'
+require 'faraday'
+require 'typhoeus'
 require 'json'
 
 module MelissaData
   module WebSmart
     class PropertyAPI
+      USER_AGENT = "MelissaData-Rubygem/#{MelissaData::VERSION}"
+      BASE_URL = 'https://property.melissadata.net'
+
+      def default_connection(url = BASE_URL, json_headers: false)
+        Faraday.new(url: url) do |conn|
+          conn.response :logger
+          conn.headers[:user_agent] = USER_AGENT
+
+          if json_headers
+            conn.headers['Content-Type'] = 'application/json'
+            conn.headers['Accept'] = 'application/json'
+          end
+
+          conn.adapter :typhoeus
+        end
+      end
+
       def property_by_apn(fips:, apn:)
-        resp = RestClient.get('https://property.melissadata.net/v4/WEB/LookupProperty/',
-                             { params: { id: MelissaData.web_smart_id,
-                                         fips: fips,
-                                         apn: apn,
-                                         format: 'json' } })
-        res = JSON.parse(resp.body).deep_transform_keys(&:underscore)
+        raw = default_connection(BASE_URL).get '/v4/WEB/LookupProperty/', {
+          id: MelissaData.web_smart_id,
+          fips: fips,
+          apn: apn,
+          format: 'json'
+        }
+
+        res = JSON.parse(raw.body).deep_transform_keys(&:underscore)
         res&.with_indifferent_access
       end
 
       def property_by_address_key(address_key:)
-        resp = RestClient.get('https://property.melissadata.net/v4/WEB/LookupProperty/',
-                             { params: { id: MelissaData.web_smart_id,
-                                         AddressKey: address_key,
-                                         format: 'json' } })
-        res = JSON.parse(resp.body).deep_transform_keys(&:underscore)
+        raw = default_connection(BASE_URL).get '/v4/WEB/LookupProperty/', {
+          id: MelissaData.web_smart_id,
+          AddressKey: address_key,
+          format: 'json'
+        }
+
+        res = JSON.parse(raw.body).deep_transform_keys(&:underscore)
         res&.with_indifferent_access
       end
 
       def address(address:, city:, state:, zip:, country:)
-        resp = JSON.parse(RestClient.get("https://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify",
-                                         { params: { id: MelissaData.web_smart_id,
-                                                     Actions: "Check",
-                                                     a1: address,
-                                                     city: city,
-                                                     state: state,
-                                                     postal: zip,
-                                                     ctry: country,
-                                                     AdvancedAddressCorrection: "on",},
-                                           accept: :json,
-                                           content_type: :json }))
-        resp["Records"].first
+        raw = default_connection(BASE_URL, json_headers: true)
+          .get '/v3/WEB/ContactVerify/doContactVerify', {
+            id: MelissaData.web_smart_id,
+            Actions: 'Check',
+            a1: address,
+            city: city,
+            state: state,
+            postal: zip,
+            ctry: country,
+            AdvancedAddressCorrection: 'on'
+        }
+
+        res = JSON.parse(raw.body).deep_transform_keys(&:underscore)
+        res&.with_indifferent_access[:records]&.first
       end
     end
   end
